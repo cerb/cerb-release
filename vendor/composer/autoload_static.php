32a761f59eb4f1e8874794388227beb487ceb41a
Jeff Standen <jeff@webgroupmedia.com>
2022-10-04 17:13:22 -0700

    [Composer] Updated Composer dependencies

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -192,8 +192,8 @@ class ComposerStaticInit80cb2a416a5e4b5562358cf92a2057b7
         ),
         'League\\Uri\\' => 
         array (
-            0 => __DIR__ . '/..' . '/league/uri-interfaces/src',
-            1 => __DIR__ . '/..' . '/league/uri/src',
+            0 => __DIR__ . '/..' . '/league/uri/src',
+            1 => __DIR__ . '/..' . '/league/uri-interfaces/src',
         ),
         'League\\OAuth2\\Server\\' => 
         array (
